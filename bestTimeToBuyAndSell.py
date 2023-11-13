5effae9854713e92ca31a2c6dc7aa1c596d74c6d
Harsh Jain <hjainofficial4@gmail.com>
2023-11-13 18:24:55 +0530

    Create bestTimeToBuyAndSell.py

---
+++ bestTimeToBuyAndSell.py
@@ -0,0 +1,22 @@
+def max_profit(prices):
+    if not prices or len(prices) == 1:
+        return 0
+
+    max_profit = 0
+    min_price = prices[0]
+
+    for price in prices:
+        min_price = min(min_price, price)
+        max_profit = max(max_profit, price - min_price)
+
+    return max_profit
+
+# Demo data
+demo_prices = [7, 1, 5, 3, 6, 4]
+
+# Calculate the result
+result = max_profit(demo_prices)
+
+# Print the input and output
+print(f"Demo data: {demo_prices}")
+print(f"The maximum profit is: {result}")
