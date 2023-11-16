2e273cd7092369b1bafd100ccd88732577f645a4
Harsh Jain <hjainofficial4@gmail.com>
2023-11-17 01:09:05 +0530

    Update WaterContainerProble.py

--- WaterContainerProble.py
+++ WaterContainerProble.py
@@ -1,18 +1,40 @@
 #BruteForce Approach
 
-class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        res = 0 ;
-        for l in range(len(height)):
-          for r in range(l+1, len(height)):
-            area = (r-l) * min(height[l] , height[r]) 
-            res = max(area, res)
+# Linear Approach 
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+      res = 0 
+      l , r = 0 , len(height) - 1 
+      while l < r:
+        area = ( r - l) * min(height[l] , height[r]) 
+        res = max(area , res) 
+
+        if height[l] < height[r] :
+          l += 1
+        else :
+          r -= 1 
+      
+      return res 
+
+
+
+# class Solution(object):
+#     def maxArea(self, height):
+#         """
+#         :type height: List[int]
+#         :rtype: int
+#         """
+#         res = 0 ;
+#         for l in range(len(height)):
+#           for r in range(l+1, len(height)):
+#             area = (r-l) * min(height[l] , height[r]) 
+#             res = max(area, res)
   
-        return res 
+#         return res 
+
+
+
           
 
 
