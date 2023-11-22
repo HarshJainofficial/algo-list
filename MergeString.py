439f6fa788facab6f990174ad44f5af5c91a6333
Harsh Jain <hjainofficial4@gmail.com>
2023-11-23 04:23:12 +0530

    Create MergeString.py

---
+++ MergeString.py
@@ -0,0 +1,23 @@
+from io import StringIO
+
+def merge_alternately(word1, word2):
+    merged_string = StringIO()
+
+    i = 0
+    j = 0
+    while i < len(word1) or j < len(word2):
+        if i < len(word1):
+            merged_string.write(word1[i])
+            i += 1
+
+        if j < len(word2):
+            merged_string.write(word2[j])
+            j += 1
+
+    return merged_string.getvalue()
+
+# Example usage
+word1 = "Hello"
+word2 = "World"
+merged_string = merge_alternately(word1, word2)
+print(merged_string)
